2a34123a17af7860bc3f8af797d2ab33d29a66ee
kundan gupta <kukundan1993@gmail.com>
2023-05-14 09:41:09 +0530

    style:updated

--- Random.js
+++ Random.js
@@ -1,11 +1,10 @@
-let api_url = "http://api.quotable.io/quotes/random";
+let api_url = "http://api.quotable.io/random";
 let Quote  = document.getElementById('Quote');
 let author = document.getElementById('author');
 
 async function getQuote(url){
    const response = await fetch(url);
    var data = await response.json();
-   console.log(data);
    Quote.innerHTML = data.author  ;
    author.innerHTML = data.content;
    
