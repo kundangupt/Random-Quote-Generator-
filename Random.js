c9e616ddddeb4c3a1f215b8d017d13729fa14828
kundan gupta <kukundan1993@gmail.com>
2023-05-14 09:21:13 +0530

    doc:updated

--- Random.js
+++ Random.js
@@ -1,13 +1,13 @@
-const api_url = "http://api.quotable.io/random";
-const Quote = document.getElementById('.Quote');
-const auther =document.getElementById('.auther');
+let api_url = "http://api.quotable.io/quotes/random";
+let Quote  = document.getElementById('Quote');
+let author = document.getElementById('author');
 
 async function getQuote(url){
-   const response = await fetch();
+   const response = await fetch(url);
    var data = await response.json();
+   console.log(data);
+   Quote.innerHTML = data.author  ;
+   author.innerHTML = data.content;
    
-   Quote.innerHTML= data.content;
-   auther.innerHTML= data.auther;
 }
-
 getQuote(api_url);
